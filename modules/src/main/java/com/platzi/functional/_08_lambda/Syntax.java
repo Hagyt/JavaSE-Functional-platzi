0a53e3cf4547c63d7773c5fc1e698608ddee10e9
David Urrego <david.urrego@trycore.com>
2024-01-30 18:41:53 -0500

    feat(Lambda-Syntax): Se agrega comentario sobre la sintaxis para las lambdas

--- modules/src/main/java/com/platzi/functional/_08_lambda/Syntax.java
+++ modules/src/main/java/com/platzi/functional/_08_lambda/Syntax.java
@@ -6,8 +6,11 @@ import java.util.function.Function;
 import java.util.function.Supplier;
 
 public class Syntax {
+    /**
+     * The lambda functions are used for samall cases, short and specific logic.
+     */
     private static void sintaxis() {
-        //Una funcion puede ser definida de la siguiente manera:
+        // Una funcion puede ser definida de la siguiente manera:
         Function<String, String> funFunction = s -> "";
 
 
@@ -22,7 +25,7 @@ public class Syntax {
 
 
 
-        //Y como vimos en la leccion anterior, si queremos ser muy especificos con el tipo:
+        // Y como vimos en la leccion anterior, si queremos ser muy especificos con el tipo:
         Function<String, String> boringFunction = (String s) -> "";
 
 
@@ -36,7 +39,7 @@ public class Syntax {
 
 
 
-        //Tambien hemos mencionado que si nuestra funcion es muy extensa podemos usar {}
+        // Tambien hemos mencionado que si nuestra funcion es muy extensa podemos usar {}
         Function<String, String> notFun = s -> {
             System.out.println("Ejecutando… 1");
             System.out.println("Ejecutando… 2");
@@ -57,11 +60,11 @@ public class Syntax {
 
 
 
-        //La variante empieza cuando tenemos mas de un parametro, pues nos vemos obligados
-        //a agrupar los parametros entre parentesis.
+        // La variante empieza cuando tenemos mas de un parametro, pues nos vemos obligados
+        // a agrupar los parametros entre parentesis.
         BiFunction<String, String, Integer> biFunFunction = (xs, s) -> 0;
 
-        //Incluso si ponemos el tipo explisito
+        // Incluso si ponemos el tipo explicito
         BiFunction<String, String, Integer> notFunBiFunction = (String xs, String s) -> 0;
 
 
@@ -84,8 +87,8 @@ public class Syntax {
 
 
 
-        //Si nuestra funcion (un Consumer tambien es una funcion) no hace nada,
-        //Java nos exige que usemos { } como cuerpo de nuestra funcion.
+        // Si nuestra funcion (un Consumer tambien es una funcion) no hace nada,
+        // Java nos exige que usemos { } como cuerpo de nuestra funcion.
         Consumer<String> consumer = s -> { };
 
 
